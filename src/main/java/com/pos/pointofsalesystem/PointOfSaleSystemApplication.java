75676c554c8c463b3f9da22322da5f29e1c83ba4
Kevin Ha <kevvintrongha@gmail.com>
2023-12-04 12:24:55 -0800

    Template "test commit"

---
+++ src/main/java/com/pos/pointofsalesystem/PointOfSaleSystemApplication.java
@@ -0,0 +1,15 @@
+package com.pos.pointofsalesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSaleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSaleSystemApplication.class, args);
+
+		System.out.println("Hello");
+	}
+
+}
